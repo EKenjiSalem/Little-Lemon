4b4d65614186cfd0ccfa8882e9165182d14a1646
Elias K. Salem <48525998+EKenjiSalem@users.noreply.github.com>
2023-07-06 10:10:52 -0700

    new components

---
+++ little-lemon-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Nav from './Nav';
+
+function App() {
+  return (
+    <>
+      <h1> Hello World </h1>
+      <Nav />
+    </>
+  );
+}
+
+export default App;
---
+++ little-lemon-app/src/Main.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function Main() {
+  return (
+    
+
+  );
+}
+
+export default Main;
